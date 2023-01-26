629f2f955fe79f4d76f626c0038ecaeea61fe495
Damonlh <myorderrecipt@gmail.com>
2023-01-26 22:08:00 +0000

    Commit

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -15,6 +15,32 @@ public class FizzBuzz {
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
     public String FizzBuzzGen(int n){
-        return "";
-    }
+
+        if(n % 3 == 0)
+        {
+            if(n % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else
+            {
+                return "Fizz";
+            }
+        }
+        else if(n % 5 == 0)  
+        {
+            if(n % 3 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else{
+                return "Buzz";
+            }
+        }
+        else
+        {
+            return "";
+        }
+
+     }
 }
